fcdab8353421f2bff9d45f7a3263a3769165f7b7
Nadia Burnasheva <nadezhda.burnasheva@jetbrains.com>
2019-08-14 11:48:33 +0200

    add failing test (fix)

--- spec/should_fail_spec.rb
+++ spec/should_fail_spec.rb
@@ -1,12 +1,12 @@
 require_relative './../message'
 
 RSpec.describe Message, "#hello_world" do
-    it 'should return hello world as default message' do
+    it 'should return hello world as default message fail' do
         message = Message.new
         expect(message.print).to eq('Hello World!')
     end
 
-    it 'should allow message in constructor' do
+    it 'should allow message in constructor fail' do
         message = Message.new('How do you do?')
         expect(message.print).to eq('How do you do')
     end
